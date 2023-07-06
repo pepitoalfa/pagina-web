5686ef7ff83784a4b3bf3e9306e8124fdaf13ca5
pepitoalfa <134400016+pepitoalfa@users.noreply.github.com>
2023-07-06 14:17:39 -0300

    Add files via upload

---
+++ proyectopaginaweb/fondo.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container')
+const colors = ['#CB51EE', '#0073BC', '#38B75E', '#D365C']
+
+const figures = () => {
+    for(let i = 0; i <= 25; i++)
+
+
+
+}
